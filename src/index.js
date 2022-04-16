c90a5db74ef7a8a5979a9836009faf7582858bd4
rtavares <ricardotavaresit@gmail.com>
2022-04-16 16:39:11 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const port = 3333;
+
+const app = express();
+app.use(express.json())
+
+const customers = [];
+/**
+* @param {String} - cpf
+* @param {number} - name
+* @param {number} - uuid
+*/
+app.post("/account", (request, response) => {
+    const {cpf, name} = request.body;
+
+    const customerAlreadyExists = customers.some( (customer) => customer.cpf === cpf);
+
+    if( customerAlreadyExists ){
+        return response.status(400).json({error: "Customer already exists!"});
+    }
+
+    customers.push({
+        cpf,
+        name, 
+        id: uuidv4(), 
+        statment:[]
+    });
+    return response.status(201).send();
+});
+
+
+/**
+* @returns {String} - 200 OK
+* @returns {String} - 400 Customer not found
+*/
+app.get("/account", (request, response) => {
+    const { cpf } = request.headers;
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if( !customer ){
+        return response.status(400).json({error: "Customer not found"});
+    }
+    return response.status(200).json(customer.statment);
+});
+
+app.listen(port, () => console.log(`Running at: ${port}`))
