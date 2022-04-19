7131ca9ed3b9e5c379ca3195ca6b11997ba963b6
rtavares <ricardotavaresit@gmail.com>
2022-04-19 20:15:53 +0100

    create getBalance and withdraw method

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ app.use(express.json())
 const customers = [];
 
 //Middleware 
-
 function verifyIfExistsAccountCPF(request, response, next){
     const { cpf } = request.headers;
 
@@ -21,6 +20,21 @@ function verifyIfExistsAccountCPF(request, response, next){
     request.customer = customer;
     return next();
 }
+
+
+function getBalance(statement){
+
+    const balance = statement.reduce((acc, operation) => {
+        if( operation.type === "credit"){
+            return acc + operation.amount;
+        }else{
+            return acc - operation.amount;
+        }
+    }, 0);
+    
+    return balance;
+}
+
 
 
 /**
@@ -77,6 +91,30 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
 
     return response.status(201).send();
 });
+
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if( balance < amount ){
+        return response.status(400).json({error: "Insufficient funds."});
+    } 
+
+    const statementOperation = {
+        amount, 
+        create_at: new Date(), 
+        type: "debit"
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
+
 
 
 
